c035fb5de9f883a610bdc87d813f9d7470cd00a8
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-06-17 16:19:42 +0700

    refactor retinaface

--- models/retinaface/models/retinaface.py
+++ models/retinaface/models/retinaface.py
@@ -1,14 +1,19 @@
 import torch
 import torch.nn as nn
+import numpy as np
 import torchvision.models.detection.backbone_utils as backbone_utils
 import torchvision.models._utils as _utils
 import torch.nn.functional as F
 from collections import OrderedDict
 
-from models.net import MobileNetV1 as MobileNetV1
-from models.net import FPN as FPN
-from models.net import SSH as SSH
+from .net import MobileNetV1 as MobileNetV1
+from .net import FPN as FPN
+from .net import SSH as SSH
 
+from ..utils.loader import load_model
+from ..layers.functions.prior_box import PriorBox
+from ..utils.box_utils import decode, decode_landm
+from ..utils.nms.py_cpu_nms import py_cpu_nms
 
 
 class ClassHead(nn.Module):
@@ -53,6 +58,7 @@ class RetinaFace(nn.Module):
         """
         super(RetinaFace,self).__init__()
         self.phase = phase
+        self.cfg = cfg
         backbone = None
         if cfg['name'] == 'mobilenet0.25':
             backbone = MobileNetV1()
@@ -125,3 +131,76 @@ class RetinaFace(nn.Module):
         else:
             output = (bbox_regressions, F.softmax(classifications, dim=-1), ldm_regressions)
         return output
+
+    def detect_faces(self, img_raw, confidence_threshold=0.9, top_k=5000, nms_threshold=0.4, keep_top_k=750, resize=1):
+        img = np.float32(img_raw)
+        im_height, im_width = img.shape[:2]
+        scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+        img -= (104, 117, 123)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.to(self.device)
+        scale = scale.to(self.device)
+
+        # tic = time.time()
+        with torch.no_grad():
+            phase = self.phase
+            self.phase = 'test'
+            loc, conf, landms = self.forward(img)  # forward pass
+            self.phase = phase
+            # print('net forward time: {:.4f}'.format(time.time() - tic))
+
+        priorbox = PriorBox(self.cfg, image_size=(im_height, im_width))
+        priors = priorbox.forward()
+        priors = priors.to(self.device)
+        prior_data = priors.data
+        boxes = decode(loc.data.squeeze(0), prior_data, self.cfg['variance'])
+        boxes = boxes * scale / resize
+        boxes = boxes.cpu().numpy()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        landms = decode_landm(landms.data.squeeze(0), prior_data, self.cfg['variance'])
+        scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2]])
+        scale1 = scale1.to(self.device)
+        landms = landms * scale1 / resize
+        landms = landms.cpu().numpy()
+
+        # ignore low scores
+        inds = np.where(scores > confidence_threshold)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][:top_k]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, nms_threshold)
+        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+        dets = dets[keep, :]
+        landms = landms[keep]
+
+        # keep top-K faster NMS
+        dets = dets[:keep_top_k, :]
+        landms = landms[:keep_top_k, :]
+        # print(landms.shape)
+        landms = landms.reshape((-1, 5, 2))
+        # print(landms.shape)
+        landms = landms.transpose((0, 2, 1))
+        # print(landms.shape)
+        landms = landms.reshape(-1, 10, )
+        # print(landms.shape)
+
+        return dets, landms
+
+
+def retinaface_mnet(pretrained=False, phase='train'):
+    return load_model('mnet', pretrained, phase)
+
+def retinaface_rnet(pretrained=False, phase='train'):
+    return load_model('rnet', pretrained, phase)
