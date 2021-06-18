08f9d2afc3b3c34532278b04b2d75098e99f2b72
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-06-18 17:30:23 +0700

    fix bug face recognition

--- models/FaceRecogniton.py
+++ models/FaceRecogniton.py
@@ -4,27 +4,24 @@ from .retinaface.models.retinaface import retinaface_mnet
 from facenet_pytorch import InceptionResnetV1
 
 class FaceRecognition(nn.Module):
-    def __init__(self, facedetector, facenet):
+    def __init__(self, facedetector, facerecognition):
         super(FaceRecognition, self).__init__()
         self.facedetector = facedetector
-        self.facenet = facenet
+        self.facerecognition = facerecognition
 
     def forward(self, image): 
-        #TODO: what type of image 
-        boxes, confidents, landmarks = self.facedetector(image)
-        #TODO: what type of image
+        boxes, confidents, landmarks = self.facedetector.detect_faces(image)
         faces = []
         for box in boxes: 
-            box = box.to('cpu').tolist()
             box = [round(x) for x in box]
             #TODO: check type box
-            face = torch.unsqueeze(image[box[0]:box[2], box[1]:box[3]], 0)
+            face = image[:, :, box[0]:box[2], box[1]:box[3]]
             face = nn.functional.interpolate(face, size=(160, 160))
-            faces.append(face.sequeeze)
+            faces.append(face.squeeze())
 
         faces = torch.stack(faces)
 
-        out = self.facenet(faces)
+        out = self.facerecognition(faces)
         _, pred = torch.max(out, 1)
 
         return boxes, pred
@@ -34,4 +31,3 @@ def facerecognition_retinaface_facenet(pretrained=False):
     facenet = InceptionResnetV1(pretrained='vggface2')
 
     return FaceRecognition(retinaface, facenet)
-
