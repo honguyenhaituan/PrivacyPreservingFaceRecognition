45936091447695cb20558555ce294a8eb67ec6c9
Hồ Nguyễn Hải Tuấn <honguyenhaituan@gmail.com>
2021-07-12 14:36:28 +0700

    add norm last layer arcface & cosface

--- models/extractor.py
+++ models/extractor.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from .backbones import get_model
 from facenet_pytorch import InceptionResnetV1
 
@@ -8,16 +9,21 @@ def fixed_image_standardization(image_tensor):
     return processed_tensor
 
 class Extractor(nn.Module):
-    def __init__(self, model, input_size, transform):
+    def __init__(self, model, input_size, transform, norm=False):
         super(Extractor, self).__init__()
         self.model = model
         self.input_size = input_size
         self.transform = transform
         self.margin = 14
+        self.norm = norm
 
     def forward(self, image):
         image = self.transform(image)
-        return self.model(image)
+        feature = self.model(image)
+        if self.norm: 
+            return F.normalize(feature, p=2, dim=-1)
+        else:
+            return feature
 
 def FaceNetExtractor(pretrained='vggface2'): 
     facenet = InceptionResnetV1(pretrained)
@@ -32,7 +38,7 @@ def ArcFaceExtractor(backbone='r18'):
     arcface = get_model(backbone, fp16=False)
     state_dict = torch.hub.load_state_dict_from_url(path)
     arcface.load_state_dict(state_dict)
-    return Extractor(arcface, (112, 112), fixed_image_standardization)
+    return Extractor(arcface, (112, 112), fixed_image_standardization, norm=True)
 
 def CosFaceExtractor(backbone='r18'):
     if backbone == 'r18':
@@ -43,7 +49,7 @@ def CosFaceExtractor(backbone='r18'):
     cosface = get_model(backbone, fp16=False)
     state_dict = torch.hub.load_state_dict_from_url(path)
     cosface.load_state_dict(state_dict)
-    return Extractor(cosface, (112, 112), fixed_image_standardization)
+    return Extractor(cosface, (112, 112), fixed_image_standardization, norm=True)
 
 def get_extractor(name):
     if name == 'facenet':
